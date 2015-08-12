dfe07dc977dd9cb25116043563e1448d0d3404d6
Tony Bowden <tony@tmtm.com>
2015-08-12 13:01:25 +0100

    Less verbosity

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,5 @@ require 'wikidata/fetcher'
 
 WikiData::Category.new('Kategoria:Deputowani do Izby Reprezentantów Zgromadzenia Narodowego Republiki Białorusi V kadencji', 'pl').wikidata_ids.each do |id|
   data = WikiData::Fetcher.new(id: id).data('en', 'be', 'ru', 'pl') or next
-  puts data
   ScraperWiki.save_sqlite([:id], data)
 end
