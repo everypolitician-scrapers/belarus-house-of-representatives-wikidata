ba08847efeadb7b965597cb0cca0c67ea2f0e1fd
Tony Bowden <tony@tmtm.com>
2015-11-14 11:03:46 +0000

    belarus-house-of-representatives-wikidata: refresh data

--- scraper.rb
+++ scraper.rb
@@ -8,3 +8,6 @@ WikiData::Category.new('Kategoria:Deputowani do Izby Reprezentantów Zgromadzeni
   data = WikiData::Fetcher.new(id: id).data('en', 'be', 'ru', 'pl') or next
   ScraperWiki.save_sqlite([:id], data)
 end
+
+require 'rest-client'
+warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
