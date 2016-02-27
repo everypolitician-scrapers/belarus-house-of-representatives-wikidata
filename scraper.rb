095903262a6c75844547beb66f68f1bfff7e0672
Tony Bowden <tony@tmtm.com>
2016-02-27 16:10:59 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
 
-WikiData::Category.new('Kategoria:Deputowani do Izby Reprezentantów Zgromadzenia Narodowego Republiki Białorusi V kadencji', 'pl').wikidata_ids.each do |id|
-  data = WikiData::Fetcher.new(id: id).data('en', 'be', 'ru', 'pl') or next
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-require 'rest-client'
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = WikiData::Category.new('Kategoria:Deputowani do Izby Reprezentantów Zgromadzenia Narodowego Republiki Białorusi V kadencji', 'pl').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: [], pl: names })
