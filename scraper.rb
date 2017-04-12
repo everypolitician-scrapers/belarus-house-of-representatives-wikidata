49ea9f841e5a0ddd5baa9251d369782af8aed3a1
Tony Bowden <tony@tmtm.com>
2017-04-12 15:08:28 +0100

    Add Term 6 + also scrape in Belarussian

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,10 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new('Kategoria:Deputowani do Izby Reprezentantów Zgromadzenia Narodowego Republiki Białorusi V kadencji', 'pl').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: [], pl: names })
+pl = WikiData::Category.new('Kategoria:Deputowani do Izby Reprezentantów Zgromadzenia Narodowego Republiki Białorusi V kadencji', 'pl').member_titles |
+     WikiData::Category.new('Kategoria:Deputowani do Izby Reprezentantów Zgromadzenia Narodowego Republiki Białorusi VI kadencji', 'pl').member_titles
+
+be = WikiData::Category.new('Катэгорыя:Дэпутаты Палаты Прадстаўнікоў Нацыянальнага сходу Рэспублікі Беларусь 5-га склікання', 'be').member_titles |
+     WikiData::Category.new('Катэгорыя:Дэпутаты Палаты Прадстаўнікоў Нацыянальнага сходу Рэспублікі Беларусь 6-га склікання', 'be').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: [], be: be, pl: pl })
